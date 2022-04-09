4e958d4f7a2f0b0f078971dc7af2e4949e8ae57c
qiyoudaoyi <qiyoudaoyi@gmail.com>
2022-04-09 19:36:25 +0800

    stack permutation

--- HW2/part1_1/main.cpp
+++ HW2/part1_1/main.cpp
@@ -1,23 +1,49 @@
 #include <iostream>
 #include <stack>
 
+int PermutationCount;
+
+enum OPERATION
+{
+    POP_INIT,
+    POP_MID,
+};
+
+inline void PrintStack(std::stack<int> s)
+{
+    for (; !s.empty(); s.pop())
+        std::cout << s.top() << " \n"[s.size() == 1 ? 1 : 0];
+}
+
+void SwitchRailroad(std::stack<int> init,
+                    std::stack<int> mid = std::stack<int>{},
+                    std::stack<int> dest = std::stack<int>{},
+                    int op = OPERATION::POP_INIT)
+{
+    if (op == OPERATION::POP_INIT) {
+        mid.push(init.top());
+        init.pop();
+    } else if (op == OPERATION::POP_MID) {
+        dest.push(mid.top());
+        mid.pop();
+    }
+
+    if (!init.empty()) SwitchRailroad(init, mid, dest, OPERATION::POP_INIT);
+    if (!mid.empty()) SwitchRailroad(init, mid, dest, OPERATION::POP_MID);
+    if (init.empty() && mid.empty()) {
+        std::cout << "Stack Permutation #" << ++PermutationCount << ": ";
+        PrintStack(dest);
+    }
+}
+
 int main()
 {
-    std::stack<int> vertical_track, left_track, right_track;
+    std::cout << "When n = 3\n";
+    SwitchRailroad(std::stack<int>({3, 2, 1}));
 
-    right_track = std::stack<int>({4, 3, 2, 1});
-    while (!right_track.empty() || !vertical_track.empty()) {
-        if (!right_track.empty()) {
-            int right_top = right_track.top();
-            vertical_track.push(right_top);
-            right_track.pop();
-        }
-        if (!vertical_track.empty()) {
-            int vertical_top = vertical_track.top();
-            left_track.push(vertical_top);
-            vertical_track.pop();
-        }
-    }
+    PermutationCount = 0;
+    std::cout << "\nWhen n = 4\n";
+    SwitchRailroad(std::stack<int>({4, 3, 2, 1}));
 
     return 0;
 }
